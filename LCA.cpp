91003fc42661727556e4090391bcdb96a4d115ae
Ankit Kumar Singh <68580104+singhankit333@users.noreply.github.com>
2021-05-18 16:45:44 +0530

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -17,11 +17,15 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* node1, TreeNode* node2)
 
 // Binary Tree
 
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return NULL;
-        if(root==p || root==q) return root;
-        TreeNode *left = lowestCommonAncestor(root->left, p, q);
-        TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        if(left && right) return root;
-        return left==NULL ? right : left;
-    }
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(!root) return NULL;
+    if(root==p || root==q) return root;
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+    if(left && right) return root;
+    return left==NULL ? right : left;
+}
+
+
+
+// One simple and straightforward approach is find "root to Node" path and then find first common path node.
